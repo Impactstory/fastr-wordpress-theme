c060760b374ec77914160d5f6b72e125a03e36d8
Jason Priem <jp@jasonpriem.org>
2014-04-01 18:43:50 -0700

    add search and social media to footer

--- footer.php
+++ footer.php
@@ -15,15 +15,27 @@
         <div class="container">
             <div>
                 <nav id="site-navigation" class="main-navigation" role="navigation">
-			        <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		        </nav><!-- #site-navigation -->
+                  <div class="menu">
+                     <ul>
+                        <li><a href="http://impactstory.org">Impactstory home</a></li>
+                        <li><a href="http://twitter.com/impactstory">Twitter</a></li>
+                        <li><a href="http://facebook.com/Impactstory">Facebook</a></li>
+                        <li><a href="https://plus.google.com/100164144715192552027/posts">g+</a></li>
+                     </ul>
+                  </div>
+              </nav><!-- #site-navigation -->
             </div>
-		    <div class="site-info">
-			    <?php do_action( 'fastr_credits' ); ?>
-			    <?php printf( __( 'Proudly powered by %s', 'fastr' ), '<a href="http://wordpress.org/" rel="generator"  target="_blank">WordPress</a>' ); ?>
-			    <span class="sep"> | </span>
-			    <?php printf( __( 'Theme: %1$s by %2$s.', 'fastr' ), 'Fastr', '<a href="http://kanishkkunal.in" rel="designer" target="_blank">Kanishk</a>' ); ?>
-		    </div><!-- .site-info -->
+
+            <?php include(TEMPLATEPATH . "/searchform.php"); ?>
+
+
+            <div class="site-info">
+             <a href="http://wordpress.org">WordPress</a>
+             <span class="sep"> | </span>
+             <a href="https://github.com/total-impact/fastr-wordpress-theme">Theme</a>
+             <span class="sep"> | </span>
+             <a href="http://creativecommons.org/licenses/by/4.0/legalcode">CC-BY</a>
+            </div><!-- .site-info -->
         </div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- searchform.php
+++ searchform.php
@@ -10,5 +10,4 @@
 		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'fastr' ); ?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'fastr' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'fastr' ); ?>">
 </form>
